9e967aa3c2f1f82d530014862477e95c2369557a
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-02-07 09:12:23 +0900

    Add class component, handleChange method, and props to display input

--- src/components/InputBasic.js
+++ src/components/InputBasic.js
@@ -12,9 +12,19 @@ class InputBasic extends React.Component {
   }
 
   handleChange = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
+    if (event.target.id === "name") {
+      this.setState({
+        name: event.target.value,
+      });
+    } else if (event.target.id === "email") {
+      this.setState({
+        email: event.target.value,
+      });
+    } else {
+      this.setState({
+        phone: event.target.value,
+      });
+    }
   };
 
   render() {
@@ -28,9 +38,27 @@ class InputBasic extends React.Component {
             onChange={this.handleChange}
             value={this.state.name.text}
           />
+          <label htmlFor="email">email</label>
+          <input
+            type="text"
+            id="email"
+            onChange={this.handleChange}
+            value={this.state.name.text}
+          />
+          <label htmlFor="phone">phone</label>
+          <input
+            type="text"
+            id="phone"
+            onChange={this.handleChange}
+            value={this.state.phone.text}
+          />
           <button type="submit">Display</button>
         </form>
-        <OutputBasic name={this.state.name} />
+        <OutputBasic
+          name={this.state.name}
+          email={this.state.email}
+          phone={this.state.phone}
+        />
       </div>
     );
   }
