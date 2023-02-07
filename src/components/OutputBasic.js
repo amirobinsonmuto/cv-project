e61e1aec58c240a1661db6b25b7cc62563ce16e6
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-02-07 09:12:54 +0900

    Add class component and render method to display basic information

--- src/components/OutputBasic.js
+++ src/components/OutputBasic.js
@@ -5,6 +5,8 @@ class OutputBasic extends React.Component {
     return (
       <div>
         <p>{this.props.name}</p>
+        <p>{this.props.email}</p>
+        <p>{this.props.phone}</p>
       </div>
     );
   }
