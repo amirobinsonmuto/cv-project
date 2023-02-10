5a5d7bd40f2837981db8186cba3d8dde12324ce7
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-02-10 16:21:18 +0900

    Update JSX

--- src/components/output/resume.js
+++ src/components/output/resume.js
@@ -2,7 +2,23 @@ import React from "react";
 
 class Resume extends React.Component {
   render() {
-    return <section className="resume"></section>;
+    return (
+      <section className="resume">
+        <div className="basic-info-output-area">
+          <h2>Name</h2>
+          <h3>Email</h3>
+          <h3>Phone</h3>
+        </div>
+        <div className="work-experience-output-area">
+          <h2>Work Experience</h2>
+          <div>insert</div>
+        </div>
+        <div className="education-output-area">
+          <h2>Education</h2>
+          <div>insert</div>
+        </div>
+      </section>
+    );
   }
 }
 
